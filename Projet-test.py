0238b25735fc9e88a8d628e210c10e27eb0114a2
malikabzz <93199009+malikabzz@users.noreply.github.com>
2022-02-08 00:35:32 +0100

    supprimer les doublons et les blancs

--- Projet-test.py
+++ Projet-test.py
@@ -4,16 +4,22 @@ from pyspark.sql.types import StringType
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-#import wget
+
+# import wget
 
 
 
 #tapper ces commandes sous le terminal de pycharm pour telecharger directement les fichiers a partir de leur URL
 #-------  wget https://s3.amazonaws.com/drivendata/data/7/public/4910797b-ee55-40a7-8668-10efd5c1b960.csv -O features.csv
 #le 2eme data set contient les étiquettes décrivant l'état des puits
+
+# tapper ces commandes sous le terminal de pycharm pour telecharger directement les fichiers a partir de leur URL
+# -------  wget https://s3.amazonaws.com/drivendata/data/7/public/4910797b-ee55-40a7-8668-10efd5c1b960.csv -O features.csv
+# le 2eme data set contient les étiquettes décrivant l'état des puits
+
 # ---------- wget https://s3.amazonaws.com/drivendata/data/7/public/0bf8bc6e-30d0-4c50-956a-603fc693d966.csv -O labels.csv
 
-#creation d'une spark session
+# creation d'une spark session
 sc = SparkSession.builder.master("local[*]").getOrCreate()
 
 feature = sc.read.csv("features.csv", inferSchema=True, header=True)
@@ -24,14 +30,14 @@ print(label.count())
 feature.printSchema()
 label.printSchema()
 
- #Fusionner les deux dataset
-data = feature.join(label, on = ("id") )
+# Fusionner les deux dataset
+data = feature.join(label, on=("id"))
 print(data.count())
 data.printSchema()
 
-
-#Affichage des 10 premieres lignes du dataframe
+# Affichage des 10 premieres lignes du dataframe
 data.show(n=10)
+
 
 # La fonction withColumn prend deux arguments -le nom de la nouvelle colonne et l'expression de la nouvelle colonne
 
@@ -49,8 +55,6 @@ str_cols = [item[0] for item in data.dtypes if item[1].startswith('string')]
 for cols in str_cols:
 
   data= data.withColumn(cols, trim(data[cols]))
-
-
 
 
 
